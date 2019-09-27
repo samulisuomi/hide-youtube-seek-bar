aa41b8cc66dc2a1e7fed9443514998ca123b7052
Samuli Suomi <sasuomi@users.noreply.github.com>
2019-09-28 00:38:52 +0300

    POC for hiding the seek bar on YT

--- popup.js
+++ popup.js
@@ -1,15 +1,23 @@
-let changeColor = document.getElementById('changeColor');
+let checkbox = document.getElementById('show');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
+const setSeekBarOpacity = show => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const display = show ? 'block' : 'none';
     chrome.tabs.executeScript(
       tabs[0].id,
-      { code: 'document.body.style.backgroundColor = "' + color + '";' });
+      { code: `document.getElementsByClassName("ytp-chrome-bottom")[0].style['display'] = '${display}'` }
+    );
   });
+}
+
+chrome.storage.sync.get('show', function({ show }) {
+  console.log("data", show)
+  checkbox.checked = show;
+  setSeekBarOpacity(show)
+});
+
+checkbox.onclick = function(element) {
+  const show = element.target.checked;
+  chrome.storage.sync.set({ show });
+  setSeekBarOpacity(show)
 };
